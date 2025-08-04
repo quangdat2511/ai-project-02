fc31c9eed39d5fa4b51f542a110a994e0ffa3ac5
danzgne <hoangdang.sn0704@gmail.com>
2025-08-04 12:28:40 +0700

    Thêm:     - Inference Engine.     - Thêm cách xử lý Stench, Breeze percepts     ở Agent.

--- definition.py
+++ definition.py
@@ -31,9 +31,15 @@ class Percept:
     glitter: bool = False
     bump: bool = False
     scream: bool = False
+    def __init__(self, s: bool, b: bool):
+        self.stench = s
+        self.breeze = b
+        glitter: bool = False
+        bump: bool = False
+        scream: bool = False
 
 class Agent:
-    def __init__(self):
+    def __init__(self, N: int):
         self.position = (0, 0)
         self.direction = Direction.EAST
         self.has_gold = False
@@ -41,9 +47,66 @@ class Agent:
         self.is_alive = True
         self.kb = KnowledgeBase()
         self.score = 0
+        self.size = N
 
     def get_action(self, percept: Percept) -> Optional[Action]:
         pass
+    
+    def add_percept(self, percept: Percept, x, y):
+        # if percept.breeze:
+        #     percept_literal = Literal("Breeze", x, y, False)
+        #     self.kb.add_clause(Clause([percept_literal]))
+        # elif percept.stench:
+        #     percept_literal = Literal("Stench", x, y, False)
+        #     self.kb.add_clause(Clause([percept_literal]))
+        self._add_breeze_axioms(x, y, percept.breeze)
+        self._add_stench_axioms(x, y, percept.stench)
+        # self.kb.simplify()
+
+    def _add_breeze_axioms(self, x, y, value):
+        """Breeze ⇔ có Pit ở ô kề"""
+        neighbors = self._neighbors(x,y)
+        b = Literal("Breeze", x, y, False)
+        pits = [Literal("Pit", nx, ny, False) for (nx,ny) in neighbors]
+        if value:
+            # Breeze(x,y) => (P1 v P2 v ...)
+            clause = Clause(pits)
+            self.kb.add_clause(clause)
+            # Mỗi Pit => Breeze
+            # for p in pits:
+            #     self.kb.add_clause(Clause([ -p, b ]))
+        else:
+            # NOT Breeze => tất cả các Pit kề đều False
+            for p in pits:
+                self.kb.add_clause(Clause([ -p ]))
+        
+    def _add_stench_axioms(self, x, y, value):
+        """Stench ⇔ có Wumpus ở ô kề"""
+        neighbors = self._neighbors(x,y)
+        s = Literal("Stench", x, y, False)
+        wumps = [Literal("Wumpus", nx, ny, False) for (nx,ny) in neighbors]
+        if value:
+            # Stench => (W1 v W2 v ...)
+            clause = Clause(wumps)
+            self.kb.add_clause(clause)
+            # Mỗi Wumpus => Stench
+            # for w in wumps:
+            #     self.kb.add_clause(Clause([ -w, s ]))
+        else:
+            # NOT Stench => tất cả Wumpus kề đều False
+            for w in wumps:
+                self.kb.add_clause(Clause([ -w ]))
+    
+    def _valid(self, nx, ny):
+        return 0 <= nx < self.size and 0 <= ny < self.size
+
+    def _neighbors(self, x, y):
+        for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+            nx, ny = x + dx, y + dy
+            if self._valid(nx, ny):
+                yield (nx, ny)
+    
+
 
 class Cell:
     def __init__(self, has_pit: bool = False, has_wumpus: bool = False, has_gold: bool = False):
@@ -76,24 +139,117 @@ class WumpusWorld:
         pass
 
 
+class Literal:
+    def __init__(self, name, x, y, negated):
+        self.name = name
+        self.position = (x, y)   # ví dụ ('1','2')
+        self.is_negated = negated
 
+    # def __repr__(self):
+    #     prefix = '¬' if self.is_negated else ''
+    #     args_str = ",".join(map(str, self.args))
+    #     return f"{prefix}{self.name}({args_str})"
+    
+    def __str__(self):
+        s = f"{self.name}{self.position}" if self.position else self.name
+        return f"~{s}" if self.is_negated else s
+
+    def __neg__(self):
+        return Literal(self.name, self.position[0], self.position[1], not self.is_negated)
+
+    def __eq__(self, other):
+        return (self.name, self.position, self.is_negated) == (other.name, self.position, other.is_negated)
+
+    def __hash__(self):
+        return hash((self.name, self.position, self.is_negated))
+
+class Clause:
+    def __init__(self, literals):
+        self.literals = set(literals)
+
+    # def __repr__(self):
+    #     return " ∨ ".join(map(str, self.literals))
+    
+    def __str__(self):
+        return " v ".join(str(lit) for lit in self.literals)
+    
+    def is_empty(self):
+        return len(self.literals) == 0
 
 class KnowledgeBase:
     def __init__(self):
-        # Facts about cells (using sets for efficiency)
-        self.visited = set()
-        self.safe = {(0, 0)}  # Start is always safe
-        self.has_pit = set()
-        self.no_pit = {(0, 0)}
-        self.has_wumpus = set()
-        self.no_wumpus = {(0, 0)}
-        
-        # Percept history
-        self.stench_at = set()
-        self.breeze_at = set()
-        self.no_stench_at = set()
-        self.no_breeze_at = set()
+        self.clauses = set()
 
-    def infer(self):
+    def simplify_clause(self, clause):
+        unit_literals = {next(iter(c.literals)) for c in self.clauses if len(c.literals) == 1}
+        new_literals = set()
+
+        for lit in clause.literals:
+            if -lit in unit_literals:
+                continue  # loại literal mâu thuẫn với fact
+            else:
+                new_literals.add(lit)
+
+        if new_literals:
+            return Clause(new_literals)
+        else:
+            return None 
+
+    def add_clause(self, clause):
+        # self.clauses.add(clause)
+        simplified_clause = self.simplify_clause(clause)
+        if simplified_clause:
+            self.clauses.add(simplified_clause)
+
+    def infer(self, query):
+        negated_query = Clause([-query])
+        new_clauses = set()
+        kb_clauses = set(self.clauses.union({negated_query}))
+        while True:
+            pairs = [ (c1, c2) for c1 in kb_clauses for c2 in kb_clauses if c1 != c2 ]
+            for (ci, cj) in pairs:
+                resolvents = self.resolve(ci, cj)
+                for res in resolvents:
+                    if res.is_empty():
+                        return True  # derived empty clause, proven
+                    new_clauses.add(res)
+            if new_clauses.issubset(kb_clauses):
+                return False  # no progress
+            kb_clauses |= new_clauses
         pass  # Logic to infer knowledge from rules
-    
+
+    def resolve(self, c1, c2):
+        resolvents = set()
+        for l1 in c1.literals:
+            for l2 in c2.literals:
+                if l1.name == l2.name and l1.position == l2.position and l1.is_negated != l2.is_negated:
+                    new_literals = (c1.literals.union(c2.literals)) - {l1, l2}
+                    resolvents.add(Clause(new_literals))
+        return resolvents
+
+    # def simplify(self):
+    #     simplified = set()
+    #     unit_literals = set()
+
+    #     # 1. Thu thập các facts
+    #     for clause in self.clauses:
+    #         if len(clause.literals) == 1:
+    #             unit_literals.add(next(iter(clause.literals)))
+    #             simplified.add(clause)
+
+    #     # 2. Duyệt các clause còn lại
+    #     for clause in self.clauses:
+    #         if len(clause.literals) == 1:
+    #             continue  # đã xử lý rồi
+
+    #         new_literals = set()
+    #         for lit in clause.literals:
+    #             if -lit in unit_literals:
+    #                 continue  # literal bị phủ định → loại bỏ
+    #             else:
+    #                 new_literals.add(lit)
+
+    #         if new_literals:
+    #             simplified.add(Clause(new_literals))
+
+    #     self.clauses = simplified
