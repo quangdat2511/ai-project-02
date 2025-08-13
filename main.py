8e0cc74f801e35fddb2e3cd2a0a2c5083743021c
Anh Khoa <nhakhoa23@clc.fitus.edu.vn>
2025-08-13 20:40:41 +0700

    clean code (delete folder execute and example)

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # Main game
 from gui import *
+
 if __name__ == "__main__":
     main()
-# Debugging
-# from execute import *
-# if __name__ == "__main__":
-#     run_and_save_results(N=8, K=2, p=0.1, mode='normal', M=2)
-#     # run_and_save_results(N=8, K=2, p=0.1, mode='advanced', M=2)
-    
-#     # run_and_save_results(N=4, K=2, p=0.1, mode='advanced', M=5)
